4a38450f5b5718f113736d93aadbebf844658c0a
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-08-10 23:00:00 +0530

    Updated Solution - GfG to GitHub

--- Medium/Longest Common Subsequence/longest-common-subsequence.cpp
+++ Medium/Longest Common Subsequence/longest-common-subsequence.cpp
@@ -10,53 +10,54 @@ class Solution
 {
     public:
     //Function to find the length of longest common subsequence in two strings.
-    vector<vector<int>>dp;
-    int solve(int i,int j,string &s1,string &s2,int &x,int &y){
+    int solveTab(int &x, int &y, string &s1, string &s2){
         
-        if(j>=y){
-            return 0;
-        }
-        if(i>=x){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
+
+        vector<int> curr1(y+1,0);
+
+        vector<int> next1(y+1,0);
         
-        int ans1=0;
-        int ans2=0;
-        int ans3=0;
-        if(s1[i]==s2[j]){
-            ans1=1+solve(i+1,j+1,s1,s2,x,y);
+        for(int i=x-1;i>=0;i--){
+            for(int j=y-1;j>=0;j--){
+                int ans1=0;
+                int ans2=0;
+                int ans3=0;
+                if(s1[i]==s2[j]){
+                    ans1=1+next1[j+1];
+                }
+                else{
+                    ans2=0+curr1[j+1];
+                    ans3=0+next1[j];
+                }
+                int ans=max(ans1,max(ans2,ans3));
+                curr1[j]= ans;
+            }
+            next1=curr1;
         }
-        else{
-            ans2=0+solve(i,j+1,s1,s2,x,y);
-            ans3=0+solve(i+1,j,s1,s2,x,y);
-        }
-        int ans=max(ans1,max(ans2,ans3));
-        return dp[i][j]= ans;
+        return curr1[0];
     }
     int lcs(int &x, int &y, string &s1, string &s2)
     {
         // your code here
-        dp.resize(x,vector<int>(y,-1));
-        return solve(0,0,s1,s2,x,y);
+        
+        return solveTab(x,y,s1,s2);
     }
 };
+
 
 
 //{ Driver Code Starts.
 int main()
 {
-    int t,n,k,x,y;
+    int t,n,m;
     cin>>t;
     while(t--)
     {
-        cin>>x>>y;          // Take size of both the strings as input
+        cin>>n>>m;          // Take size of both the strings as input
         string s1,s2;
         cin>>s1>>s2;        // Take both the string as input
         Solution ob;
-        cout << ob.lcs(x, y, s1, s2) << endl;
+        cout << ob.lcs(n, m, s1, s2) << endl;
     }
     return 0;
 }
