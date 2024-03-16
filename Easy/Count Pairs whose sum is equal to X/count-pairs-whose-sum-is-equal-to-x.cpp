dbf22dd5f6e99a40a335799d62138dac3c4ddd65
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2024-03-17 00:11:40 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count Pairs whose sum is equal to X/count-pairs-whose-sum-is-equal-to-x.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void append(struct Node** headRef, struct Node** tailRef, int newData) {
+    struct Node* new_node = new Node(newData);
+
+    if (*headRef == NULL) {
+        *headRef = new_node;
+    }
+
+    else {
+        (*tailRef)->next = new_node;
+    }
+    *tailRef = new_node;
+}
+
+void printList(struct Node* head) {
+    while (head) {
+        cout << head->data << ' ';
+        head = head->next;
+    }
+}
+
+
+// } Driver Code Ends
+/*
+Structure of the node of the linked list is as
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution{
+  public:
+    // your task is to complete this function
+    int countPairs(struct Node* head1, struct Node* head2, int x) {
+        // Code here
+         unordered_set<int>s;
+        Node *p=head1;
+        Node *q=head2;
+        while(p!=NULL)
+        {
+            s.insert(p->data);
+            p=p->next;
+        }
+        int count =0;
+        while(q!=NULL)
+        {
+            if(s.find(x-q->data)!=s.end())
+            {
+                count++;
+            }
+            q=q->next;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        struct Node* head1 = NULL;
+        struct Node* tail1 = NULL;
+        struct Node* tail2 = NULL;
+        struct Node* head2 = NULL;
+        int n1, n2, tmp, x;
+        cin >> n1;
+        while (n1--) {
+            cin >> tmp;
+            append(&head1, &tail1, tmp);
+        }
+        cin >> n2;
+        while (n2--) {
+            cin >> tmp;
+            append(&head2, &tail2, tmp);
+        }
+        cin >> x;
+        Solution obj;
+        cout << obj.countPairs(head1, head2, x) << '\n';
+    }
+    return 0;
+}
+// } Driver Code Ends
